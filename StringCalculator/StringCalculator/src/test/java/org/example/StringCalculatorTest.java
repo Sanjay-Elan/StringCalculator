b82f8ea814353a3010b19dfc35ce305cb8d0bb8d
SanjayElanGCIT <sanjay.elavazhagan@gcitsolutions.com>
2023-06-15 16:34:31 +0530

    Adding code for handling custom delimitters with two extract methods getsplit and stringarraytonumber conversation

---
+++ StringCalculator/StringCalculator/src/test/java/org/example/StringCalculatorTest.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class StringCalculatorTest {
+
+    @Test
+    void WhenEmptyStringPassedResultShouldBeZero() {
+        var calc = new StringCalculator();
+        Assertions.assertThat(calc.Add("")).isEqualTo(0);
+    }
+
+    @Test
+    void SameValueShouldBeReturnForOneNumber() {
+        var calc = new StringCalculator();
+        Assertions.assertThat(calc.Add("1")).isEqualTo(1);
+    }
+
+    @Test
+    void SumOfTwoNumbersShouldBeReturnForTwoNumbers() {
+        var calc = new StringCalculator();
+        Assertions.assertThat(calc.Add("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    void SumOfUnknownNumbersShouldBeReturnForUnknownNumbers() {
+        var calc = new StringCalculator();
+        Assertions.assertThat(calc.Add("1,2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void SumOfNumbersShouldBeReturnForNumbersGivenWithCommaAndNewLine() {
+        var calc = new StringCalculator();
+        Assertions.assertThat(calc.Add("1\n2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void SumOfNumbersShouldBeReturnForNumbersGivenWithCustomDelimiters() {
+        var calc = new StringCalculator();
+        Assertions.assertThat(calc.Add("//;\n1;2;3")).isEqualTo(6);
+    }
+}
